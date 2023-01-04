090023fd3a7d14e72da35082c2e8f8588de5bc10
HEKEPOIU <decnovpk92805@gmail.com>
2023-01-04 14:42:26 +0800

    test

---
+++ JS/Animate.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    lottie.loadAnimation({
+        wrapper: animate,
+        animType: 'svg',
+        autoplay: true,
+        loop: true,
+        path: '../json/3d.json'
+      });
+
+
+})
