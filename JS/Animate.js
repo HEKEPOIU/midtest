bb247061442472717d76d0ce1c8e1d7df88b5845
HEKEPOIU <decnovpk92805@gmail.com>
2023-01-05 21:56:52 +0800

    15

--- JS/Animate.js
+++ JS/Animate.js
@@ -1,11 +1,23 @@
 $(document).ready(function () {
+  try{
     lottie.loadAnimation({
         wrapper: animate,
         animType: 'svg',
         autoplay: true,
         loop: true,
         path: '../json/3d.json'
-      });
-
+    });
+  }
+  catch{}
+  try{
+    lottie.loadAnimation({
+        wrapper: animate1,
+        animType: 'svg',
+        autoplay: true,
+        loop: true,
+        path: '../json/2d.json'
+    });
+  }
+  catch{}
 
 })
