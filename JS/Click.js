2c014c7540f7658945646f464e26f7e7a6297443
HEKEPOIU <decnovpk92805@gmail.com>
2023-01-12 07:11:28 +0800

    112

--- JS/Click.js
+++ JS/Click.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
     
     let information;
     let img;
-    try
-    {
-        $('.bigwork_workboxlist').slick();
-    } catch (error)
-    {
+    // try
+    // {
+    //     $('.bigwork_workboxlist').slick();
+    // } catch (error)
+    // {
         
-    }
+    // }
     $(".W_workbox_Like").click(function () {
         $(this).toggleClass("W_workbox_Likefull");
 
@@ -18,6 +18,7 @@ $(document).ready(function () {
         information = $(this).parent().children("div").children("div").children("p").text();
         img = $(this).parent().children(".W_workbox_T").attr("class").split(/\s+/);
         let a = img[1] + " " + information;
+        // alert(a);
         document.cookie = a;
 
     })
