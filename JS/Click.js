c468729065c806dc6bda995e9f2b2dc3382aa3bb
HEKEPOIU <decnovpk92805@gmail.com>
2023-01-12 10:44:50 +0800

    end fuck

--- JS/Click.js
+++ JS/Click.js
@@ -2,6 +2,49 @@ $(document).ready(function () {
     
     let information;
     let img;
+    let nowpic = 0;
+    // alert(piclist[0] .html);
+
+    $("#next").click(function () {
+        nowpic++;
+        if (nowpic == 1)
+        {
+            $(".W_workbox_other").addClass("offset");
+        }
+        if (nowpic == 2)
+        {
+            $(".W_workbox_other").addClass("offset1");
+        }
+        if (nowpic == 3)
+        {
+            $(".W_workbox_other").addClass("offset2");
+        }
+        if (nowpic == 4)
+        {
+            nowpic = 0
+            $(".W_workbox_other").removeClass("offset").removeClass("offset1").removeClass("offset2").addClass("offset0");
+        }
+    })
+    $("#pre").click(function () {
+        nowpic--;
+        if (nowpic == 2)
+        {
+            $(".W_workbox_other").removeClass("offset2");
+        }
+        if (nowpic == 1)
+        {
+            $(".W_workbox_other").removeClass("offset1");
+        }
+        if (nowpic == 0)
+        {
+            $(".W_workbox_other").removeClass("offset");
+        }
+        if (nowpic == -1)
+        {
+            nowpic = 3
+            $(".W_workbox_other").addClass("offset").addClass("offset1").addClass("offset2");
+        }
+    })
     // try
     // {
     //     $('.bigwork_workboxlist').slick();
